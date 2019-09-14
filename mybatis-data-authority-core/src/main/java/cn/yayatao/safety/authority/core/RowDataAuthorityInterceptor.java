19bd3293ffe0ef924d972753208aacb11f3ec9cf
fireflyhoo <fireflyhoo@gmail.com>
2019-09-14 21:28:36 +0800

    初始化信息

--- mybatis-data-authority-core/src/main/java/cn/yayatao/safety/authority/core/RowDataAuthorityInterceptor.java
+++ mybatis-data-authority-core/src/main/java/cn/yayatao/safety/authority/core/RowDataAuthorityInterceptor.java
@@ -1,16 +1,25 @@
 package cn.yayatao.safety.authority.core;
 
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
+import java.sql.Connection;
 import java.util.Properties;
 
 
+@Intercepts(
+        { @Signature(type = Executor.class, method = "query", args = {MappedStatement.class,Object.class, RowBounds.class, ResultHandler.class}) }
+)
 public class RowDataAuthorityInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        return null;
+        System.out.println(invocation);
+        Object proceed = invocation.proceed();
+        return proceed;
     }
 
     @Override
